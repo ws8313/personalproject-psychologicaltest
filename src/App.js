89f684b7562da0976121b3dc76925cf32abfcad3
ws8313 <ws8313@naver.com>
2022-06-23 02:16:15 +0900

    UserPage css flex

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Router } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -16,62 +16,64 @@ const UserPage = ({ history }) => {
     }, [name, gender]);
     
     return (
-        <div id="inputform">
-            <h1 id="h1">직업가치관검사</h1>
+        <div className="container">
+            <div id="inputform">
+                <h1 id="h1">직업가치관검사</h1>
 
-                {/* 사용자 이름, 성별 입력 form  */}
-                {/* 11.21 수정 내용 이름 input 자동완성기능 off */}
-                <form>
-                <div>
-                    <p id="p">이름</p>
-                    <input 
-                        type="text" 
-                        id="name"
-                        placeholder="이름을 입력하세요"
-                        autoComplete='off'
-                        value={name} 
-                        onChange={
-                            (event) => {
-                                setName(event.target.value);
-                                console.log(name);
+                    {/* 사용자 이름, 성별 입력 form  */}
+                    {/* 11.21 수정 내용 이름 input 자동완성기능 off */}
+                    <form>
+                    <div>
+                        <p id="p">이름</p>
+                        <input 
+                            type="text" 
+                            id="name"
+                            placeholder="이름을 입력하세요"
+                            autoComplete='off'
+                            value={name} 
+                            onChange={
+                                (event) => {
+                                    setName(event.target.value);
+                                    console.log(name);
+                                }
                             }
-                        }
-                    />
-                    <p id="p">성별</p>
-                    <label id="userlabel" for="male"><input 
-                        type="radio" 
-                        id="male" 
-                        name="gender" 
-                        value="100323"
-                        onChange={
-                            (event) => {
-                                setGender(event.target.value);
+                        />
+                        <p id="p">성별</p>
+                        <label id="userlabel" for="male"><input 
+                            type="radio" 
+                            id="male" 
+                            name="gender" 
+                            value="100323"
+                            onChange={
+                                (event) => {
+                                    setGender(event.target.value);
 
+                                }
                             }
-                        }
-                    />남자</label>
-                    <label id="userlabel" for="female"><input 
-                        type="radio" 
-                        id="female" 
-                        name="gender" 
-                        value="100324"
-                        onChange={
-                            (event) => {
-                                setGender(event.target.value);
+                        />남자</label>
+                        <label id="userlabel" for="female"><input 
+                            type="radio" 
+                            id="female" 
+                            name="gender" 
+                            value="100324"
+                            onChange={
+                                (event) => {
+                                    setGender(event.target.value);
+                                }
                             }
-                        }
-                    />여자</label>
-                </div>
+                        />여자</label>
+                    </div>
 
-                {/* 이름, 성별 둘 중 하나라도 입력하지 않으면 버튼 비활성화 */}
-                <div id="button">
-                    { 
-                        (name.length === 0) || (gender.length === 0) ?
-                        <button id="userbtn" type="submit" class="btn btn-primary disabled" disabled>검사시작</button> :
-                        <button id="userbtn" type="submit" class="btn btn-primary" onClick={ () => { history.push("/TestExPage") }}>검사시작</button>
-                    }
-                </div>
-            </form>
+                    {/* 이름, 성별 둘 중 하나라도 입력하지 않으면 버튼 비활성화 */}
+                    <div id="button">
+                        { 
+                            (name.length === 0) || (gender.length === 0) ?
+                            <button id="userbtn" type="submit" class="btn btn-primary disabled" disabled>검사시작</button> :
+                            <button id="userbtn" type="submit" class="btn btn-primary" onClick={ () => { history.push("/TestExPage") }}>검사시작</button>
+                        }
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
