07ee4e1f922cf71510202ffec2af16d5c156d3da
ws8313 <ws8313@naver.com>
2022-06-23 03:01:50 +0900

    TestExPage css, progress percent

--- src/components/TestExPage.js
+++ src/components/TestExPage.js
@@ -1,79 +1,99 @@
 import React, { useState } from "react";
 import ProgressBar from "react-bootstrap/ProgressBar";
 
-
 const TestExPage = ({ history }) => {
-    const [answer, setAnswer] = useState('');
+  const [answer, setAnswer] = useState("");
 
-    return (
+  return (
+    <div>
+      <div>
+        <h1 id="h1">검사예시</h1>
+      </div>
+
+      <div>
+        <div id="per">{answer === "1" || answer === "2" ? 100 : 0}%</div>
+      </div>
+
+      <div>
+        <ProgressBar
+          variant="progress-bar_color"
+          animated
+          now={answer === "1" || answer === "2" ? 100 : 0}
+        />
+      </div>
+
+      <div id="q">
+        직업과 관련된 두개의 가치 중에서 자기에게 더 중요한 가치에 표시하세요.
+        <br />
+        가치의 뜻을 잘모르겠다면 문항 아래에 있는 가치의 설명을 확인해보세요.
+      </div>
+
+      <form>
         <div>
-            <div>
-                <h1 id="h1">검사예시</h1>
-            </div>
+          <label id="exlabel" for="answer1">
+            <input
+              type="radio"
+              id="answer1"
+              name="answer"
+              value="1"
+              onChange={(event) => {
+                setAnswer(event.target.value);
+              }}
+            />
+            능력발휘
+          </label>
 
-            <div>
-                <div id="per">0%</div>
-            </div>
-
-            <div>
-                <ProgressBar animated now={50} />
-            </div>
-
-            <div id="q">
-                직업과 관련된 두개의 가치 중에서 자기에게 더 중요한 가치에 표시하세요.
-                <br/>
-                가치의 뜻을 잘모르겠다면 문항 아래에 있는 가치의 설명을 확인해보세요.
-            </div>
-
-            <form>
-                <div>
-                    <label id="exlabel" for="answer1">
-                    <input
-                        type="radio"
-                        id="answer1"
-                        name="answer"
-                        value="1"
-                        onChange={
-                            (event) => {
-                                setAnswer(event.target.value);
-                            }
-                        }
-                    /> 능력발휘
-                    </label>
-
-                    <label id="exlabel" for="answer2">
-                    <input
-                        type="radio"
-                        id="answer2"
-                        name="answer"
-                        value="2"
-                        onChange={
-                            (event) => {
-                                setAnswer(event.target.value);
-                            }
-                        }
-                    /> 자율성
-                    </label>
-                </div>
-                <div id="btnbox">
-                    <div>
-                        <button id="btn" class="btn btn-primary" onClick={() => history.goBack()}>이전</button>
-                    </div>
-
-                    <div>
-                        {
-                            (answer === '1') || (answer === '2') ?
-                            <button id="btn" type="submit" class="btn btn-primary" onClick={ (event) => { history.push("/TestPage/0") }}>검사시작</button>
-                            :
-                            <button id="btn" type="submit" class="btn btn-primary disabled" disabled>검사시작</button>
-                        }
-                    </div>
-                </div>
-            </form>
+          <label id="exlabel" for="answer2">
+            <input
+              type="radio"
+              id="answer2"
+              name="answer"
+              value="2"
+              onChange={(event) => {
+                setAnswer(event.target.value);
+              }}
+            />
+            자율성
+          </label>
         </div>
+        <div id="btnbox">
+          <div>
+            <button
+              id="btn"
+              className="btn btn-primary"
+              onClick={() => history.goBack()}
+            >
+              이전
+            </button>
+          </div>
 
-        
-    )
-}
+          <div>
+            {answer === "1" || answer === "2" ? (
+              <button
+                id="btn"
+                type="submit"
+                className="btn btn-primary"
+                onClick={(event) => {
+                  history.push("/TestPage/0");
+                }}
+              >
+                검사시작
+              </button>
+            ) : (
+              <button
+                id="btn"
+                type="submit"
+                className="btn btn-primary disabled"
+                disabled
+              >
+                검사시작
+              </button>
+            )}
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default TestExPage;
--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -3,79 +3,94 @@ import { UserContext } from "./UserStore";
 
 // 사용자 정보 받아오기 및 경고창으로 확인
 const UserPage = ({ history }) => {
-    const [name, setName] = useState('')
-    const [gender, setGender] = useState('')
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
 
-    const context = useContext(UserContext);
-    localStorage.clear();
+  const context = useContext(UserContext);
+  localStorage.clear();
 
-    useEffect(() => {
-        context.name = name;
-        context.gender = gender;
-        console.log(context)
-    }, [name, gender]);
-    
-    return (
-        <div className="container">
-            <div id="inputform">
-                <h1 id="h1">직업가치관검사</h1>
+  useEffect(() => {
+    context.name = name;
+    context.gender = gender;
+    console.log(context);
+  }, [name, gender]);
 
-                    {/* 사용자 이름, 성별 입력 form  */}
-                    {/* 11.21 수정 내용 이름 input 자동완성기능 off */}
-                    <form>
-                    <div>
-                        <p id="p">이름</p>
-                        <input 
-                            type="text" 
-                            id="name"
-                            placeholder="이름을 입력하세요"
-                            autoComplete='off'
-                            value={name} 
-                            onChange={
-                                (event) => {
-                                    setName(event.target.value);
-                                    console.log(name);
-                                }
-                            }
-                        />
-                        <p id="p">성별</p>
-                        <label id="userlabel" for="male"><input 
-                            type="radio" 
-                            id="male" 
-                            name="gender" 
-                            value="100323"
-                            onChange={
-                                (event) => {
-                                    setGender(event.target.value);
+  return (
+    <div className="container">
+      <div id="inputform">
+        <h1 id="h1">직업가치관검사</h1>
 
-                                }
-                            }
-                        />남자</label>
-                        <label id="userlabel" for="female"><input 
-                            type="radio" 
-                            id="female" 
-                            name="gender" 
-                            value="100324"
-                            onChange={
-                                (event) => {
-                                    setGender(event.target.value);
-                                }
-                            }
-                        />여자</label>
-                    </div>
+        {/* 사용자 이름, 성별 입력 form  */}
+        {/* 11.21 수정 내용 이름 input 자동완성기능 off */}
+        <form>
+          <div>
+            <p id="p">이름</p>
+            <input
+              type="text"
+              id="name"
+              placeholder="이름을 입력하세요"
+              autoComplete="off"
+              value={name}
+              onChange={(event) => {
+                setName(event.target.value);
+                console.log(name);
+              }}
+            />
+            <p id="p">성별</p>
+            <label id="userlabel" for="male">
+              <input
+                type="radio"
+                id="male"
+                name="gender"
+                value="100323"
+                onChange={(event) => {
+                  setGender(event.target.value);
+                }}
+              />
+              남자
+            </label>
+            <label id="userlabel" for="female">
+              <input
+                type="radio"
+                id="female"
+                name="gender"
+                value="100324"
+                onChange={(event) => {
+                  setGender(event.target.value);
+                }}
+              />
+              여자
+            </label>
+          </div>
 
-                    {/* 이름, 성별 둘 중 하나라도 입력하지 않으면 버튼 비활성화 */}
-                    <div id="button">
-                        { 
-                            (name.length === 0) || (gender.length === 0) ?
-                            <button id="userbtn" type="submit" class="btn btn-primary disabled" disabled>검사시작</button> :
-                            <button id="userbtn" type="submit" class="btn btn-primary" onClick={ () => { history.push("/TestExPage") }}>검사시작</button>
-                        }
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
-}
+          {/* 이름, 성별 둘 중 하나라도 입력하지 않으면 버튼 비활성화 */}
+          <div id="button">
+            {name.length === 0 || gender.length === 0 ? (
+              <button
+                id="userbtn"
+                type="submit"
+                className="btn btn-primary disabled"
+                disabled
+              >
+                검사시작
+              </button>
+            ) : (
+              <button
+                id="userbtn"
+                type="submit"
+                className="btn btn-primary"
+                onClick={() => {
+                  history.push("/TestExPage");
+                }}
+              >
+                검사시작
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default UserPage;
