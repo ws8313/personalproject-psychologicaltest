80dbda6bb4050fd66ae2468debc3464d69ead404
ws8313 <ws8313@naver.com>
2022-06-24 04:22:34 +0900

    TestEndPage container

--- src/components/TestEndPage.js
+++ src/components/TestEndPage.js
@@ -2,31 +2,33 @@ import React from "react";
 
 const TestEndPage = ({ history }) => {
   return (
-    <div>
-      <div>
-        <h1 id="h1">검사가 완료되었습니다.</h1>
-      </div>
-
-      <div>
-        <div id="q">
-          검사결과는 여러분이 직업을 선택할 때 상대적으로 어떠한 가치를 중요하게
-          생각하는지를 알려주고,
-          <br />
-          중요 가치를 충족시켜줄 수 있는 직업에 대해 생각해 볼 기회를
-          제공합니다.
+    <div className="container">
+      <div className="testend_container">
+        <div>
+          <h1 className="testend_h1">검사가 완료되었습니다.</h1>
         </div>
-      </div>
 
-      <div id="btnbox">
-        <button
-          id="btn"
-          class="btn btn-primary"
-          onClick={() => {
-            history.push("/ResultPage");
-          }}
-        >
-          결과보기
-        </button>
+        <div>
+          <div id="q">
+            검사결과는 여러분이 직업을 선택할 때 상대적으로 어떠한 가치를
+            중요하게 생각하는지를 알려주고,
+            <br />
+            중요 가치를 충족시켜줄 수 있는 직업에 대해 생각해 볼 기회를
+            제공합니다.
+          </div>
+        </div>
+
+        <div id="btnbox">
+          <button
+            id="btn"
+            class="btn btn-primary"
+            onClick={() => {
+              history.push("/ResultPage");
+            }}
+          >
+            결과보기
+          </button>
+        </div>
       </div>
     </div>
   );
